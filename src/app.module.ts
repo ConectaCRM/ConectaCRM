6f30cc083fe51cfeefc5748127d07174dd998625
Bobeats . <gabriel.gomes86@gmail.com>
2025-12-11 16:47:07 -0300

    Classe Usuario Criada, métodos CRUD criados

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number (process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Usuario],
+      synchronize: true,
+      logging: true, 
+    }),
+    UsuarioModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,28 @@
+import { IsNotEmpty, MinLength } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: "tb_usuarios"})
+export class Usuario {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @Column({ length: 100, nullable: false })
+    nome: string
+
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    email: string
+    
+    @IsNotEmpty()
+    @MinLength (8)
+    @Column({length:30, nullable: false })
+    senha: string
+
+    @IsNotEmpty()
+    @Column ({ length: 1000, nullable: false})
+    foto: string
+
+
+}
